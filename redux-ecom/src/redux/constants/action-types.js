d7cec8de21005fa456cab7500a2b2856174fa873
sanjeevkumar-works <107302749+sanjeevkumar-works@users.noreply.github.com>
2022-06-11 15:43:25 +0530

    Delete redux-ecom directory

--- redux-ecom/src/redux/constants/action-types.js
+++
@@ -1,7 +0,0 @@
-const ActionTypes = {
-    ADD_PRODUCT: "ADD_PRODUCT",
-    REMOVE_PRODUCT: "REMOVE_PRODUCT",
-    DECREASE_QTY: "DECREASE_QTY"
-}
-
-export default ActionTypes;
--- redux-ecom/src/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import cartReducer from "./cartReducer";
-
-
-export const rootreducer = combineReducers({
-    cart: cartReducer,
-})
